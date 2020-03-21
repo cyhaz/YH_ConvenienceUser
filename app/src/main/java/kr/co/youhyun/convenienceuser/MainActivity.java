22f491042fab921738f3f2d04d5b03e7297429c5
cyhaz <cyhaz@naver.com>
2020-03-21 12:55:54 +0900

    프로젝트 기초 세팅  - BaseActivity 생성 및 멤버변수 생성  - build.gradle 데이터바인딩 세팅  - main xml 기초 세팅(데이터바인딩 준비)  - MainActivity에서 데이터바인딩 및 오버라이딩

--- app/src/main/java/kr/co/youhyun/convenienceuser/MainActivity.java
+++ app/src/main/java/kr/co/youhyun/convenienceuser/MainActivity.java
@@ -1,14 +1,31 @@
 package kr.co.youhyun.convenienceuser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import kr.co.youhyun.convenienceuser.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
     }
 }
