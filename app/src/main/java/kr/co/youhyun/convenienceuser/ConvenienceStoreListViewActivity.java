5eab0402c9b331c482af7c0d5cf1f85496162358
cyhaz <cyhaz@naver.com>
2020-03-22 14:41:44 +0900

    리스트뷰 화면 생성 및 편의점데이터 변수 생성자/게터/세터 세팅

--- app/src/main/java/kr/co/youhyun/convenienceuser/ConvenienceStoreListViewActivity.java
+++ app/src/main/java/kr/co/youhyun/convenienceuser/ConvenienceStoreListViewActivity.java
@@ -1,14 +1,31 @@
 package kr.co.youhyun.convenienceuser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
-public class ConvenienceStoreListViewActivity extends AppCompatActivity {
+import kr.co.youhyun.convenienceuser.databinding.ActivityConvenienceStoreListViewBinding;
+
+public class ConvenienceStoreListViewActivity extends BaseActivity {
+
+    ActivityConvenienceStoreListViewBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_convenience_store_list_view);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_convenience_store_list_view);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
     }
 }
--- app/src/main/java/kr/co/youhyun/convenienceuser/datas/ConvenienceStore.java
+++ app/src/main/java/kr/co/youhyun/convenienceuser/datas/ConvenienceStore.java
@@ -1,4 +1,40 @@
 package kr.co.youhyun.convenienceuser.datas;
 
-public class ConvenienceStore {
+import java.io.Serializable;
+
+public class ConvenienceStore implements Serializable {
+
+    private String storeName;
+    private String storePhoneNum;
+    private String logoUrl;
+
+    public ConvenienceStore(String storeName, String storePhoneNum, String logoUrl) {
+        this.storeName = storeName;
+        this.storePhoneNum = storePhoneNum;
+        this.logoUrl = logoUrl;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getStorePhoneNum() {
+        return storePhoneNum;
+    }
+
+    public void setStorePhoneNum(String storePhoneNum) {
+        this.storePhoneNum = storePhoneNum;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
 }
