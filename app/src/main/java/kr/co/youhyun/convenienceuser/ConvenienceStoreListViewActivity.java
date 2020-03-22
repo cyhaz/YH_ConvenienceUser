0c0edbe3475430be42ffa2be7b0c80bda2f7dab8
cyhaz <cyhaz@naver.com>
2020-03-22 15:59:16 +0900

    리스트뷰 데이터 작성

--- app/src/main/java/kr/co/youhyun/convenienceuser/ConvenienceStoreListViewActivity.java
+++ app/src/main/java/kr/co/youhyun/convenienceuser/ConvenienceStoreListViewActivity.java
@@ -38,28 +38,25 @@ public class ConvenienceStoreListViewActivity extends BaseActivity {
 
                 ConvenienceStore clickedStore = convenienceStores.get(position);
 
-                Intent intent = new Intent(mContext, )
+                Intent intent = new Intent(mContext, DetailPageActivity.class);
+                intent.putExtra("store", clickedStore);
+                startActivity(intent);
             }
         });
-
-
-//        binding.pizzaListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//
-//                PizzaStore clickedStore = pizzaStores.get(position);
-//
-//                Intent intent = new Intent(mContext, PizzaStoreDetailActivity.class);
-//                intent.putExtra("store", clickedStore);
-//                startActivity(intent);
-//
-//            }
-//        });
-
     }
 
     @Override
     public void setValues() {
 
+        convenienceStoreAdapter = new ConvenienceStoreAdapter(mContext, R.layout.convenience_store_list_item, convenienceStores);
+        binding.convenienceListView.setAdapter(convenienceStoreAdapter);
+
+        convenienceStores.add(new ConvenienceStore("CU", "02)1111-1111","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQTXhZnkuZP4eIlZAgqQse0Uicn3a5_FA9Fsvbi-CPrw5ipdz9_"));
+        convenienceStores.add(new ConvenienceStore("GS25", "02)2222-2222","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSF4YV78yIE0xtQ_p5d6qmxpD5EL8GdX_hRrZ2cIGDlaewJM610"));
+        convenienceStores.add(new ConvenienceStore("emart24", "02)3333-3333","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQC-F_1ZZDtVNp4riwwBUIJLA5eA9U1K-7aUIBxxBqIMjzVWOvm"));
+        convenienceStores.add(new ConvenienceStore("7 eleven", "02)4444-4444","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRcsaKTxotxvxX8uBNpDTguLfliujs8Y8y3yOHZHfFavOqZonUq"));
+        convenienceStores.add(new ConvenienceStore("MINI STOP", "02)5555-5555","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQH4SQTZppv2ahJwxI7OHZk41Cy79LKToJ519UvkZK_n_6RaLg5"));
+        convenienceStores.add(new ConvenienceStore("Store Away", "02)6666-6666","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQKfkua88UwF3gLD-0yw5Kw90AVYh1bovY3pR2mjdrgLkn8NXJO"));
+        convenienceStoreAdapter.notifyDataSetChanged();
     }
 }
